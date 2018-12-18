43da1082a8452a8b87905463c284fc9d043e38fb
Dan Rue <dan.rue@linaro.org>
2018-12-17 21:26:28 -0600

    Clean up find_intermittent_issues
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- find_intermittent_issues.py
+++ find_intermittent_issues.py
@@ -8,43 +8,6 @@ import get_data
 
 cache_path = './haystack'
 
-def get_all_test_names(cache_path):
-    ''' Read all tests.json files, and gather a list of all tests '''
-    tests = []
-    for entry in os.listdir(os.path.join(cache_path, 'testruns')):
-        if not os.path.isdir(os.path.join(cache_path, 'testruns', entry)):
-            continue
-        with open(os.path.join(cache_path, 'testruns', entry, 'tests.json')) as f:
-            results = json.load(f)
-        for result in results:
-            if result['name'] not in tests:
-                tests.append(result['name'])
-    tests.sort()
-    return tests
-
-def get_builds_from_branch(branch, cache_path):
-    ''' Read all tests.json files, and gather a list of all tests '''
-    builds = []
-    for entry in os.listdir(os.path.join(cache_path, 'builds')):
-        if not os.path.isdir(os.path.join(cache_path, 'testruns', entry)):
-            continue
-        with open(os.path.join(cache_path, 'testruns', entry, 'tests.json')) as f:
-            results = json.load(f)
-        for result in results:
-            if result['name'] not in tests:
-                tests.append(result['name'])
-    tests.sort()
-    return tests
-
-
-def find_intermittent_failures_on_mainline(branch, cache_path, transition_threshold=3):
-    '''
-    On mainline, read the test data for the last 30 builds and find tests which
-    have transitioned (from fail to pass or from pass to fail) more than 3
-    times. Such tests can be said to be intermittent.
-    '''
-    builds = []
-
 class get_env_slug:
     def __init__(self):
         self.urls = {}
@@ -59,12 +22,14 @@ class get_env_slug:
 
 if __name__ == '__main__':
     tests_transitions = {} # Dictionary of test names and transition counts
+    threshold = 2 # Find tests that have changed state more than this many times
+    builds = 10 # Look through this many builds
+    branch = '4.4' # Look through builds on this branch
 
-    project_id = get_data.projects['4.4']
+    project_id = get_data.projects[branch]
     qareports = get_data.qareports(get_data.cache_path)
     builds_url = qareports.get_object(get_data.urljoiner(get_data.squad, 'api', 'projects', project_id))['builds']
     get_env = get_env_slug()
-    count = 10
     for build in qareports.get_objects(builds_url):
         for testrun in qareports.get_objects(build['testruns']):
             tests = qareports.get_leaf_objects(testrun['tests'])
@@ -79,11 +44,10 @@ if __name__ == '__main__':
                 # state transition
                 if test['status'] != tests_transitions[test['name']][env_slug]['status']:
                     tests_transitions[test['name']][env_slug]['count'] += 1
-        count -= 1
-        if count <= 0:
+        builds -= 1
+        if builds <= 0:
             break
 
-    threshold = 1
     for test, envs in tests_transitions.items():
         for env, data in envs.items():
             if data['count'] > threshold:
